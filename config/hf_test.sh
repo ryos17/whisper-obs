27777e52d688b247ddaeb978a6f586acb6d5f2cb
ryota <ryotasato2005@gmail.com>
2025-09-29 17:24:20 +0000

    sorted files

--- config/hf_test.sh
+++ config/hf_test.sh
@@ -5,13 +5,13 @@ torchrun --nproc_per_node=${ngpu} train/fine-tune_on_hf_dataset.py \
 --sampling_rate 16000 \
 --num_proc 16 \
 --train_strategy epoch \
---learning_rate 3e-3 \
---warmup 10 \
+--learning_rate 3.75e-5 \
+--warmup 25 \
 --train_batchsize 128 \
 --eval_batchsize 64 \
 --num_epochs 10 \
 --resume_from_ckpt None \
---output_dir op_dir_steps \
+--output_dir output/hf_test \
 --train_datasets ahazeemi/librispeech10h ahazeemi/librispeech10h \
 --train_dataset_splits train.10 validation \
 --train_dataset_text_columns text text \
